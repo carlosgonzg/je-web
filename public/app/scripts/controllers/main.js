8e1146b536cabf4a82dc4a08b0fbbdaccb6f3de6
Carlos Gonzalez <cgonzalez@simetricaconsulting.com>
2017-01-04 10:47:10 -0500

    agregando angularjs

---
+++ public/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jarturaExpressApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jarturaExpressApp
+ */
+angular.module('jarturaExpressApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
